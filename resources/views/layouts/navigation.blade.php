9551cd9433ffa6127ab324885b020d9fb19a85f4
ToniAnttonen01 <toni.anttonen@hotmail.com>
2024-03-22 10:17:09 +0200

    haloo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,17 +15,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-
-                    <a href="/">
-                        <x-application-logo class="w-20 h-10 fill-current text-gray-500 " />
-                    </a>
                 </div>
                 </div>
             </div>

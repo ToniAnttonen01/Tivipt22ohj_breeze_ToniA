cf6f86d9a547856db372e01c598acb2ab5277fed
ToniAnttonen01 <toni.anttonen@hotmail.com>
2024-03-26 11:15:17 +0200

    LoginNappi_valmis-Korjaus_dashboardiin-Matkat_sivu_aloitettu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,20 +1,21 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-16 w-7">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center text-white">
                     <a href="{{ route('dashboard') }}">
                         Matkatoimisto
                     </a>
                 </div>
 
-                <div>
+                <div class="flex items-center justify-between h-19 ">
 
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"><a href="{{ route('login') }}">Login</a></button>
                 </div>
                 </div>
             </div>
